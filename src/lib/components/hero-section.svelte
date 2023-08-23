6aad7c78a59fb66b4d824657f0b2e1f6e46aa118
harshmangalam <harshdev8218@gmail.com>
2023-08-23 10:48:33 +0530

    added daisyui hero component

---
+++ src/lib/components/hero-section.svelte
@@ -0,0 +1,26 @@
+<script>
+  import CreateRoom from "./CreateRoom.svelte";
+  import JoinRoom from "./JoinRoom.svelte";
+</script>
+
+<div class="hero bg-base-200">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="max-w-lg w-full">
+      <img src="hero.svg" alt="mockup" />
+    </div>
+    <div>
+      <div class="text-5xl">
+        <h1>Open source video meetings.</h1>
+        <h1>Now free for everyone.</h1>
+      </div>
+      <p class="py-6 text-lg">
+        An open source and free to use one-to-one private video calling web app
+        build with sveltekit.
+      </p>
+      <div class="flex items-center gap-4 flex-col sm:flex-row">
+        <CreateRoom />
+        <JoinRoom />
+      </div>
+    </div>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,40 +2,15 @@
   import CreateRoom from "$lib/components/CreateRoom.svelte";
   import JoinRoom from "$lib/components/JoinRoom.svelte";
   import Navbar from "$lib/components/Navbar.svelte";
+  import HeroSection from "$lib/components/hero-section.svelte";
 </script>
 
 <svelte:head>
   <title>Svelte Meet</title>
 </svelte:head>
-<div class="min-h-screen dark:bg-gray-900 p-4 dark:text-gray-200">
+<div class="min-h-screen bg-base-100">
   <Navbar />
-  <main class="py-8">
-    <section class="bg-white dark:bg-gray-900">
-      <div
-        class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12"
-      >
-        <div class="mr-auto place-self-center lg:col-span-7">
-          <h1
-            class="max-w-3xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"
-          >
-            Open source one to one video call.
-          </h1>
-          <p
-            class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
-          >
-            An open source video calling web app build with sveltekit, socketio
-            and tailwindcss
-          </p>
-
-          <div class="flex items-center gap-4 flex-col sm:flex-row">
-            <CreateRoom />
-            <JoinRoom />
-          </div>
-        </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-          <img src="hero.svg" alt="mockup" />
-        </div>
-      </div>
-    </section>
+  <main>
+    <HeroSection />
   </main>
 </div>
