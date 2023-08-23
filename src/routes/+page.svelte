137ad25c4e038635fafa37fcc84d9f6453a99615
harshmangalam <harshdev8218@gmail.com>
2023-08-23 10:53:16 +0530

    added navbar component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import CreateRoom from "$lib/components/CreateRoom.svelte";
   import JoinRoom from "$lib/components/JoinRoom.svelte";
-  import Navbar from "$lib/components/Navbar.svelte";
+  import Navbar from "$lib/components/navbar.svelte";
   import HeroSection from "$lib/components/hero-section.svelte";
 </script>
 
