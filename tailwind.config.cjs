d192bf508f757d2eacff5fea2b350804c00d9edf
harshmangalam <harshdev8218@gmail.com>
2023-08-23 10:35:10 +0530

    configure daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  darkMode:"class",
-  theme: {
-    extend: {},
-    
-  },
-  plugins: [],
-}
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  darkMode: "class",
+  plugins: [require("daisyui")],
+};
