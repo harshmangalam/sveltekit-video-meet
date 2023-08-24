4909bf9a1286789bd6732eae2cab104b109d2c6d
harshmangalam <harshdev8218@gmail.com>
2023-08-24 12:03:46 +0530

    switch to adapter node

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "svelte-adapter-deno";
+import adapter from "@sveltejs/adapter-node";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
