10679182006c78dd6246e9953cd030805083a17a
harshmangalam <harshdev8218@gmail.com>
2023-08-24 11:42:15 +0530

    added deno deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "svelte-adapter-deno";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
